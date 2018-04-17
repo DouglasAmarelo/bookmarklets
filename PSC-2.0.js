5d57695730fa916a0c328de6ea15a4ab36579a17
Douglas 'Amarelo' Lopes <d.lopes.nascimento@gmail.com>
2018-04-17 06:46:43 -0300

    Atualização de colaboradores

--- PSC-2.0.js
+++ PSC-2.0.js
@@ -13,7 +13,7 @@
 	var reporter     = document.querySelector('#reporter-val span').getAttribute('rel');
 	var assignee     = document.querySelector('#assignee-val span').getAttribute('rel');
 	var collapsedCom = document.querySelector('.collapsed-comments');
-	var authorsList  = [ 'marcos_s_melo', 'ursula.marinho', 'carol.barrios', 'marcelo.pavone' ];
+	var authorsList  = [ 'douglas', 'ursula.marinho', 'carol.barrios', 'stephani.galvani', 'marcelo.pavone' ];
 	var commentField = document.querySelector('#comment');
 	var commentText  = commentField.value;
 	var allAuthors   = '';
@@ -68,4 +68,4 @@
 })();
 
 // Bookmarklet
-javascript:void%20function(){(function(){function%20e(){var%20e=document.querySelectorAll(%22.action-details%20a%22);-1==u.indexOf(r)%26%26u.push(r);for(var%20n=0;n%3Ce.length;n++){var%20a=e[n].getAttribute(%22rel%22);-1==u.indexOf(a)%26%26a!==o%26%26u.push(a)}t()}function%20t(){for(var%20e=0;e%3Cu.length;e++){var%20t=u[e];l+=%22%20[~%22+t+%22]%20%22}}var%20r=document.querySelector(%22%23reporter-val%20span%22).getAttribute(%22rel%22),o=document.querySelector(%22%23assignee-val%20span%22).getAttribute(%22rel%22),n=document.querySelector(%22.collapsed-comments%22),u=[%22marcos_s_melo%22,%22ursula.marinho%22,%22carol.barrios%22,%22marcelo.pavone%22],a=document.querySelector(%22%23comment%22),c=a.value,l=%22%22;n%26%26n.click(),setTimeout(function(){e(),a.value=c+%22\n%22+l+%22%20PSC%22},2500)})()}();
+javascript:void%20function(){(function(){function%20e(){var%20e=document.querySelectorAll(%22.action-details%20a%22);-1==a.indexOf(r)%26%26a.push(r);for(var%20o=0,u=e.length;u%3Eo;o++){var%20l=e[o].getAttribute(%22rel%22);-1==a.indexOf(l)%26%26l!==n%26%26a.push(l)}t()}function%20t(){for(var%20e=0;e%3Ca.length;e++){var%20t=a[e];c+=%22%20[~%22+t+%22]%20%22}}var%20r=document.querySelector(%22%23reporter-val%20span%22).getAttribute(%22rel%22),n=document.querySelector(%22%23assignee-val%20span%22).getAttribute(%22rel%22),o=document.querySelector(%22.collapsed-comments%22),a=[%22douglas%22,%22ursula.marinho%22,%22carol.barrios%22,%22stephani.galvani%22,%22marcelo.pavone%22],u=document.querySelector(%22%23comment%22),l=u.value,c=%22%22;o%26%26o.click(),setTimeout(function(){e(),u.value=l+%22\n%22+c+%22%20PSC%22},2500)})()}();
