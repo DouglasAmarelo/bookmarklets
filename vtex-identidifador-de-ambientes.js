496f55164a739a36726f4be4176c891d8c3e484a
Douglas 'Amarelo' Lopes <d.lopes.nascimento@gmail.com>
2018-06-26 15:25:59 -0300

    Identificador de ambientes para o Requestly

--- vtex-identidifador-de-ambientes.js
+++ vtex-identidifador-de-ambientes.js
@@ -12,7 +12,6 @@
 		var regQA = /(qa2?\.)/gmi;
 
 		function renderBar(ambiente, hexaColor) {
-			console.log('RENDER');
 			return `
 				<div style="background: #${hexaColor}; padding: 1em; color: #000; font-weight: bold; border: 1px solid #000; font-size: 16px; font-family: 'Trebuchet MS', Tahoma;">
 					Você está no ambiente de [ ${ambiente} ]
@@ -22,14 +21,12 @@
 
 		// Prod
 		if (window.location.host.match(regProd)) {
-			console.log('PROD');
 			$body.insertBefore(prodBar, vtexTopBar);
 			document.querySelector('#jussi-bar-prod').innerHTML = renderBar('PRODUÇÃO', '44fb53');
 		}
 
 		// QA
 		if (window.location.host.match(regQA)) {
-			console.log('QA');
 			$body.insertBefore(qaBar, vtexTopBar);
 			document.querySelector('#jussi-bar-qa').innerHTML = renderBar('QA', 'eefb44');
 		}
